7cd09096c47ef042bc7574908b96d65accca5b58
Lecha91 <lecha91@hotmail.com>
2020-02-13 16:41:47 +0100

    random-color-update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-// import SingleComponent from "./components/SingleComponent";
+import SingleComponent from "./components/SingleComponent";
 
 function App() {
-  const [photos, setPhotos] = useState({});
+  const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/photos").then(res => {
@@ -14,11 +14,13 @@ function App() {
 
   return (
     <div className="App">
-      <div className="sing">
-        {photos.map(photo => (
-          <h1>{photo.title}</h1>
-        ))}
-      </div>
+      {photos.legth === 0 ? (
+        <h1>Nema nicega</h1>
+      ) : (
+        photos.map(photo => (
+          <SingleComponent id={photo.id} title={photo.title} />
+        ))
+      )}
     </div>
   );
 }
--- src/components/SingleComponent/index.js
+++ src/components/SingleComponent/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SingleComponent.css";
 
 const SingleComponent = ({ id, title }) => {
+  const [bgColor, setBgColor] = useState("");
+
+  useEffect(() => {
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    const color = "rgb(" + x + "," + y + "," + z + ")";
+    return setBgColor(color);
+  }, []);
+
   return (
-    <div className="SingleComponent" id={id}>
+    <div
+      className="SingleComponent"
+      id={id}
+      style={{ backgroundColor: `${bgColor}` }}
+    >
       <h1>{title}</h1>
     </div>
   );
